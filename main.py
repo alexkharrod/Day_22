4e6f8e4cd59bc114235ccb35090d5a8b1961e337
alexharrod <alexkharrod@gmail.com>
2024-09-01 16:49:42 -0400

    Inital Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Pong Game
+
+
+
+# import turtle and screen
+
+
+# Draw line in middle of screen
+
+
+# Create Score on left and SCore on Right
+
+
+
+# Draw Paddles
+
+# get paddles to move up and down
+
+
+# Draw ball and make it bounce of edge of  Lower screen
+
+
+# make ball bounce off of paddles
+
+
+# if paddle misses ball and it goes off screen opposite player score increases
